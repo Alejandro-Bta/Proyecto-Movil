47f25aea32f19d9eee517576eafea1b3dd155697
Alejandro-Bta <85244456+Alejandro-Bta@users.noreply.github.com>
2021-11-18 15:56:37 -0600

    Adaptando app a tipo plataforma

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text, Image } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 import RegisterForm from "../components/Auth/RegisterForm";
 import logo from "../../assets/logo.jpg";
 import { layoutStyle } from "../styles";
@@ -9,7 +16,11 @@ export default function Auth() {
   return (
     <View style={layoutStyle.container}>
       <Image style={styles.logo} source={logo} />
-      {showLogin ? <Text>FormLogin</Text> : <RegisterForm />}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        {showLogin ? <Text>FormLogin</Text> : <RegisterForm />}
+      </KeyboardAvoidingView>
     </View>
   );
 }
