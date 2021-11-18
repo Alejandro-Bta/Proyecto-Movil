1a821c8064f05fc1a49f4146e8d2031d6c69cc53
Alejandro-Bta <85244456+Alejandro-Bta@users.noreply.github.com>
2021-11-18 14:50:05 -0600

    Estilos

---
+++ src/screens/Auth.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, View, Text, SafeAreaView } from "react-native";
+
+export default function Auth() {
+  return (
+    <View>
+      <Text>Hola</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
